ce9ebe45b391a4237340b77a3fd4245d837adbdd
f0ursecond <2ez4second@gmail.com>
2022-11-02 21:06:49 +0700

    add animations

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: unnecessary_string_interpolations
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:quran_app/model.dart';
 import 'package:quran_app/repository.dart';
 
@@ -58,61 +60,77 @@ class _HomePageState extends State<HomePage> {
             future: getData(),
             builder: (context, index) {
               if (index.connectionState == ConnectionState.done) {
-                return ListView.builder(
-                    itemCount: text.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: ListTile(
-                          leading: CircleAvatar(
-                            radius: 16,
-                            backgroundColor: Color(0xFF672CBC),
-                            child: Text(
-                              '${text[index].id}',
-                              style: GoogleFonts.poppins(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.white,
+                return AnimationLimiter(
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: text.length,
+                      itemBuilder: (context, index) {
+                        return AnimationConfiguration.staggeredList(
+                          position: index,
+                          duration: const Duration(milliseconds: 375),
+                          child: SlideAnimation(
+                            child: FlipAnimation(
+                              child: Card(
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    radius: 16,
+                                    backgroundColor: Color(0xFF672CBC),
+                                    child: Text(
+                                      '${text[index].id}',
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  title: Transform(
+                                    transform:
+                                        Matrix4.translationValues(0, -5, 0),
+                                    child: Text(
+                                      '${text[index].latin}',
+                                      style: GoogleFonts.karla(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                  subtitle: Transform(
+                                    transform:
+                                        Matrix4.translationValues(0, -3, 0),
+                                    child: Text(
+                                      '${text[index].arti}',
+                                      style: GoogleFonts.karla(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                  trailing: Transform(
+                                    transform:
+                                        Matrix4.translationValues(5, -1, 0),
+                                    child: Text(
+                                      '${text[index].arab}',
+                                      style: GoogleFonts.amiri(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                  dense: true,
+                                ),
                               ),
                             ),
                           ),
-                          title: Transform(
-                            transform: Matrix4.translationValues(0, -5, 0),
-                            child: Text(
-                              '${text[index].latin}',
-                              style: GoogleFonts.karla(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          subtitle: Transform(
-                            transform: Matrix4.translationValues(0, -3, 0),
-                            child: Text(
-                              '${text[index].arti}',
-                              style: GoogleFonts.karla(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          trailing: Transform(
-                            transform: Matrix4.translationValues(5, -1, 0),
-                            child: Text(
-                              '${text[index].arab}',
-                              style: GoogleFonts.amiri(
-                                fontSize: 22,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          dense: true,
-                        ),
-                      );
-                    });
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(color: Colors.black),
+                        );
+                      }),
                 );
+              } else if (index.connectionState == ConnectionState.waiting) {
+                return Center(
+                    child: LoadingAnimationWidget.fallingDot(
+                        color: Colors.purple, size: 50));
+              } else {
+                return const Text('Check Your Connection Again');
               }
             }));
   }
